c47a71d358c7ed1841d267720588c32b7d6e5a63
Khamisi Lawrence <khamisilawrencejm@gmail.com>
2024-11-21 03:44:48 -0500

    install sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import netlify from '@astrojs/netlify';
 
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap()]
 });
