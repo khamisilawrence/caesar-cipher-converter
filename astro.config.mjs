bcc830804017e966bd60ce8bf6bfc04656bf9281
Khamisi Lawrence <khamisilawrencejm@gmail.com>
2025-01-21 19:04:07 -0500

    build: remove `astro-meta-tags` and `astro-page-insight` packages to clean up unnecessary dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,9 @@ import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
 
-import astroMetaTags from "astro-meta-tags";
-
-import pageInsight from "astro-page-insight";
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   // adapter: netlify(),
-  integrations: [tailwind(), sitemap(), astroMetaTags(), pageInsight()],
+  integrations: [tailwind(), sitemap()],
 });
