35ec75935e3257d51dabfbf6caf9af3db24d4914
Khamisi Lawrence <khamisilawrencejm@gmail.com>
2025-01-21 19:08:47 -0500

    fix: reinstall `@astro/netlify` package to resolve mismatch with version of `astro` package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
+
 import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
@@ -8,6 +10,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  // adapter: netlify(),
+  adapter: netlify(),
   integrations: [tailwind(), sitemap()],
 });
