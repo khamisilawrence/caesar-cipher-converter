3b43e261abb4d6be3f46e34082e1c8df9084efdc
Khamisi Lawrence <khamisilawrencejm@gmail.com>
2025-01-21 19:02:28 -0500

    chore: update astro config file to reflect repo cleaning process

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import netlify from '@astrojs/netlify';
+import tailwind from "@astrojs/tailwind";
 
-import tailwind from '@astrojs/tailwind';
-
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 import astroMetaTags from "astro-meta-tags";
-
-import tunnel from 'astro-tunnel';
 
 import pageInsight from "astro-page-insight";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
-  integrations: [tailwind(), sitemap(), astroMetaTags(), tunnel(), pageInsight()]
+  output: "server",
+  // adapter: netlify(),
+  integrations: [tailwind(), sitemap(), astroMetaTags(), pageInsight()],
 });
